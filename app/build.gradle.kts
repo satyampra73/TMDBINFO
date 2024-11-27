998e7748cf31f0fe194628694b1e00e1f52737c6
Satyam Prajapati <satyam7355pra@gmail.com>
2024-11-27 16:53:41 +0530

    Dependency setup done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,10 +35,35 @@ android {
         jvmTarget = "11"
     }
     buildToolsVersion = "34.0.0"
+
+    buildFeatures {
+        dataBinding = true
+    }
+
 }
 
 dependencies {
 
+    val room_version = "2.6.1"
+    val dagger_version = "2.52"
+    val coroutines_version = "1.9.0"
+    val retrofit_version = "2.11.0"
+    val okHTTP_version = "4.12.0"
+
+    implementation("androidx.room:room-runtime:$room_version")
+
+    // If this project uses any Kotlin source, use Kotlin Symbol Processing (KSP)
+    // See Add the KSP plugin to your project
+    //noinspection KaptUsageInsteadOfKsp
+    kapt("androidx.room:room-compiler:$room_version")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutines_version")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutines_version")
+    implementation("com.google.dagger:dagger:$dagger_version")
+    kapt("com.google.dagger:dagger-compiler:$dagger_version")
+    implementation("com.squareup.retrofit2:retrofit:$retrofit_version")
+    implementation("com.squareup.retrofit2:converter-gson:$retrofit_version")
+    implementation("com.squareup.okhttp3:logging-interceptor:$okHTTP_version")
+    implementation(libs.github.glide)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
